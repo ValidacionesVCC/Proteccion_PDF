f57829347719c0a49b09081af7559dec0f783b53
ValidacionesVCC <Gerencia@validacionesvcc.com>
2025-12-04 13:14:03 -0500

    Update wsgi.py

--- Proyecto_Proteccion_PDF/wsgi.py
+++ Proyecto_Proteccion_PDF/wsgi.py
@@ -1,6 +1,13 @@
+"""
+WSGI config for Proyecto_Proteccion_PDF project.
+
+This exposes the WSGI callable as a module-level variable named `application`.
+"""
+
 import os
 from django.core.wsgi import get_wsgi_application
 
+# Esta ES tu única app válida
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Proyecto_Proteccion_PDF.settings')
 
 application = get_wsgi_application()
