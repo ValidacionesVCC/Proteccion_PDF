142ee349d2d009a07779033b96b0f0be88c31b7a
ValidacionesVCC <Gerencia@validacionesvcc.com>
2025-12-04 15:00:05 -0500

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,1 +1,18 @@
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Proyecto_Proteccion_PDF.settings')
+#!/usr/bin/env python
+import os
+import sys
+
+def main():
+    """Run administrative tasks."""
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Proyecto_Proteccion_PDF.settings')
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django."
+        ) from exc
+    execute_from_command_line(sys.argv)
+
+
+if __name__ == '__main__':
+    main()
