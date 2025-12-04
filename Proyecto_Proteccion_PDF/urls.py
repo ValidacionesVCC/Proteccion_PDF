a75873a1f343f62dd832328bb6c8e0711cbe3238
ValidacionesVCC <Gerencia@validacionesvcc.com>
2025-12-04 10:13:11 -0500

    Update urls.py

--- Proyecto_Proteccion_PDF/urls.py
+++ Proyecto_Proteccion_PDF/urls.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('core.urls')),
+    # Admin opcional (no es necesario para la API pero no estorba)
+    path("admin/", admin.site.urls),
+
+    # Prefijo de la API
+    path("api/", include("core.urls")),
 ]
