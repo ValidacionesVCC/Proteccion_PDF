8029a42828c453e30493e4bed780b14b24a3cdf9
ValidacionesVCC <Gerencia@validacionesvcc.com>
2025-12-04 10:12:40 -0500

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("health/", views.health_check, name="health_check"),
+    path("health/", views.health, name="health"),
     path("proteger-pdf/", views.proteger_pdf, name="proteger_pdf"),
 ]
