c83e429e1c83aa16055f5254ff2625c1551f6930
ValidacionesVCC <Gerencia@validacionesvcc.com>
2025-12-05 10:20:14 -0500

    Update urls.py

--- Proyecto_Proteccion_PDF/urls.py
+++ Proyecto_Proteccion_PDF/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from core.views import home
 
 urlpatterns = [
+    path("", home, name="home"),   # Página raíz del servidor
     path("admin/", admin.site.urls),
     path("api/", include("core.urls")),
 ]
